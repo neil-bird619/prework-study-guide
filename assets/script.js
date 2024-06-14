eca62e81a321c9f0c8b424599d592d41d7317781
neil <boosterneil@gmail.com>
2024-06-14 11:30:31 -0400

    Add JavaScript Functionality

---
+++ assets/script.js
@@ -0,0 +1,29 @@
+const topics = ['HTML', 'CSS', 'Git', 'JavaScript'];
+
+function listTopics() {
+    for (let x = 0; x < topics.length; x++) {
+      console.log(topics[x]);
+    }
+  }
+
+  function listTopics() {
+    for (let x= 0; x < topics.length; x++) {
+        console.log(topics[x]);
+    }
+  }
+
+  function selectTopic() {
+    if (topic === 'HTML') {
+        console.log("Let's study HTML!");      
+    } else if (topic === 'CSS') {
+        console.log("Let's study CSS!");
+    } else if (topic === 'Git') {
+        console.log("Let's study Git!");
+    } else if (topic === 'JavaScript') {
+        console.log("let's study JavaScript!");
+    } else {
+        console.log('Please try again!');
+    }
+  }
+
+  listTopics() 
